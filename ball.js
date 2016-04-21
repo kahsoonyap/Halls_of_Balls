c02337c4ef7c7f1fa674ee4fa38ee616cb864c91
Kah Soon Yap <kahsoonyap1998@hotmail.com>
2016-04-21 12:33:54 -0400

    class work

--- ball.js
+++ ball.js
@@ -1,1 +1,13 @@
 console.log("hello");
+
+var ball = function(x, y){
+    var radius = 0;
+    var xcor = x;
+    var ycor = y;
+    var xspd = 0;
+    var yspd = 0;
+    
+    var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
+    c.setAttribute("x", xcor);
+    c.setAttribute("y", ycor);
+}
