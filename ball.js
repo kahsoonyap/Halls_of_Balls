367df795ed0a6b845eebb7e74784fa36cb390949
kahsoonyap <kahsoonyap1998@hotmail.com>
2016-04-21 18:00:51 -0700

    Can bounce off walls

--- ball.js
+++ ball.js
@@ -1,36 +1,66 @@
 console.log("hello");
+var xmax = 500;
+var ymax = 500;
 
-var ball = function(x, y, r){
+
+var ball = function(xcor, ycor, r, xv, yv){
     var radius = r;
-    var xcor = x;
-    var ycor = y;
-    var xspd = 0;
-    var yspd = 0;
+    var x = xcor;
+    var y = ycor;
+    var xvel = xv;
+    var yvel = yv;
+    var nextx = x + xvel;
+    var nexty = y + yvel;
     
     var s = document.getElementById("hall");
     var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
-    c.setAttribute("cx", xcor);
-    c.setAttribute("cy", ycor);
+    c.setAttribute("cx", x);
+    c.setAttribute("cy", y);
     c.setAttribute("r", r);
     c.setAttribute("fill", "red");
     c.setAttribute("stroke", "black");
     s.appendChild(c);
     
-    var getX = function() {
+    var getX = function(){
         return x;
     };
 
-    var getY = function() {
+    var getY = function(){
         return y;
     };
-
-    var getRadius = function() {
+    var getRadius = function(){
         return r;
+    };
+    
+    var setXVel = function(vel){
+	xvel = vel;
+    };
+    var setYVel = function(vel){
+	yvel = vel;
+    };
+    
+    var move = function(){
+	if (nextx - 10 <= 0 || nextx + 10 >= xmax){
+	    xvel *= -1;
+	}
+	if (nexty -10 <= 0 || nexty + 10 >= ymax){
+	    yvel *= -1;
+	}
+	x += xvel;
+	y += yvel;
+	nextx = x + xvel;
+	nexty = y + yvel;
+	c.setAttribute("cx", x);
+	c.setAttribute("cy", y);
+	console.log(nextx);	
     };
 
     return {
 	getX: getX,
 	getY: getY,
-	getR: getRadius
-    }
-}
+	getR: getRadius,
+	move: move,
+	setXVel: setXVel,
+	setYVel: setYVel
+    };
+};
