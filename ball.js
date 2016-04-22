d3012475bb8fc107ed84ab4d3a9ba6600fc9d641
kahsoonyap <kahsoonyap1998@hotmail.com>
2016-04-21 17:31:47 -0700

    circle appears and can get info on the ball

--- ball.js
+++ ball.js
@@ -1,13 +1,36 @@
 console.log("hello");
 
-var ball = function(x, y){
-    var radius = 0;
+var ball = function(x, y, r){
+    var radius = r;
     var xcor = x;
     var ycor = y;
     var xspd = 0;
     var yspd = 0;
     
+    var s = document.getElementById("hall");
     var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
-    c.setAttribute("x", xcor);
-    c.setAttribute("y", ycor);
+    c.setAttribute("cx", xcor);
+    c.setAttribute("cy", ycor);
+    c.setAttribute("r", r);
+    c.setAttribute("fill", "red");
+    c.setAttribute("stroke", "black");
+    s.appendChild(c);
+    
+    var getX = function() {
+        return x;
+    };
+
+    var getY = function() {
+        return y;
+    };
+
+    var getRadius = function() {
+        return r;
+    };
+
+    return {
+	getX: getX,
+	getY: getY,
+	getR: getRadius
+    }
 }
